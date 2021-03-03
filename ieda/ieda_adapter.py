fdc9700399798ddd22cedca50537027e23cbd9a1
Roger Dahl <git@dahlsys.com>
2021-03-03 01:15:35 -0700

    Update IEDA adapter to current production version
    This updates the IEDA adapter to the version currently running in production, which is dynamically configured and handles multiple sites.

--- ieda/ieda_adapter.py
+++ ieda/ieda_adapter.py
@@ -35,6 +35,7 @@ hold objects from EarthChem.
 import datetime
 import io
 import logging
+import os
 import pprint
 import requests
 import sys
@@ -52,57 +53,72 @@ import d1_common.xml
 
 import d1_client
 import schema_org
+import d1_common.util
+import d1_common.url
 
+from config import *
 
-IEDA_SITE_MAP = "http://get.iedadata.org/sitemaps/usap_sitemap.xml"
 SCIMETA_FORMAT_ID = 'http://www.isotc211.org/2005/gmd'
-SCIMETA_RIGHTS_HOLDER = 'CN=urn:node:mnTestIEDA,DC=dataone,DC=org'
-SCIMETA_SUBMITTER = 'CN=urn:node:mnTestIEDA,DC=dataone,DC=org'
-SCIMETA_AUTHORITATIVE_MEMBER_NODE = 'urn:node:mnTestIEDA'
-
-# BASE_URL = 'https://gmn.dataone.org/ieda'
-BASE_URL = 'https://gmn.test.dataone.org/mn'
-
-# CERT_PEM_PATH = './client_cert.pem'
-# CERT_KEY_PATH = './client_key_nopassword.pem'
-
-CERT_PEM_PATH = './urn_node_mnTestIEDA.pem'
-CERT_KEY_PATH = './urn_node_mnTestIEDA.key'
 
 NS_DICT = {
-  'gmd': 'http://www.isotc211.org/2005/gmd',
-  'gco': 'http://www.isotc211.org/2005/gco',
+    'gmd': 'http://www.isotc211.org/2005/gmd',
+    'gco': 'http://www.isotc211.org/2005/gco',
 }
 
 
 def main():
-  logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.DEBUG,
-  )
+    d1_common.util.log_setup()
 
-  resource_list = schema_org.load_resources_from_sitemap(IEDA_SITE_MAP)
+    for sitemap_idx, site_dict in enumerate(SITEMAP_LIST):
+        try:
+            proc_sitemap(sitemap_idx, len(SITEMAP_LIST), site_dict)
+        except Exception:
+            logging.exception(
+                'Unable to process sitemap. site_dict={}'.format(site_dict))
 
-  logging.info('Found resources: {}'.format(len(resource_list)))
 
-  gmn_client = create_gmn_client()
+def proc_sitemap(sitemap_idx, sitemap_count, site_dict):
+    logging.info('#' * 100)
+    logging.info('Processing site. idx={}/{}. site_dict={}'.format(
+        sitemap_idx + 1, sitemap_count, pprint.pformat(site_dict))
+    )
 
-  for resource_dict in resource_list:
-    logging.info('-' * 80)
+    sitemap_url = d1_common.url.joinPathElements(SITEMAP_ROOT, site_dict['sitemap'])
+    gmn_base_url = d1_common.url.joinPathElements(GMN_BASE_URL, site_dict['gmn_path'])
+    cert_pem_path = os.path.join(CERT_PATH_ROOT, site_dict['cert_pem_path'])
+    cert_key_path = os.path.join(CERT_PATH_ROOT, site_dict['cert_key_path'])
+
+    logging.info('sitemap_url="{}"'.format(sitemap_url))
+    logging.info('gmn_base_url="{}"'.format(gmn_base_url))
+    logging.info('cert_pem_path="{}"'.format(cert_pem_path))
+    logging.info('cert_key_path="{}"'.format(cert_key_path))
+
+    resource_list = schema_org.load_resources_from_sitemap(sitemap_url)
+
+    logging.info('Found resources: {}'.format(len(resource_list)))
+
+    gmn_client = create_gmn_client(gmn_base_url, cert_pem_path, cert_key_path)
+
+    for resource_idx, resource_dict in enumerate(resource_list):
+        try:
+            proc_resource(resource_idx, len(resource_list), resource_dict, gmn_client)
+        except Exception:
+            logging.exception(
+                'Unable to process resource. resource_idx={}'.format(resource_idx))
+
+
+def proc_resource(resource_idx, resource_count, resource_dict, gmn_client):
+    logging.info('-' * 100)
+    logging.info('Processing resource. idx={}/{}. resource_dict={}'.format(
+        resource_idx + 1, resource_count, pprint.pformat(resource_dict))
+    )
+
     entry_dict = schema_org.load_schema_org(resource_dict)
 
     result_dict = {
-      **resource_dict,
-      **entry_dict,
+        **resource_dict,
+        **entry_dict,
     }
-
-    logging.info(pprint.pformat(result_dict))
-
-    if 'error' in result_dict:
-      logging.error(
-        'error="{}" url="{}"'.format(result_dict['error'], result_dict['url'])
-      )
-      continue
 
     # {
     #   'date_modified': '2018-01-25T15:55:08-05:00',
@@ -112,128 +128,126 @@ def main():
     #   'url': 'http://get.iedadata.org/metadata/iso/609539'
     # }
 
+    logging.info('result_dict={}'.format(pprint.pformat(result_dict)))
+
+    if 'error' in result_dict:
+        logging.error(
+            'error="{}" url="{}"'.format(result_dict['error'], result_dict['url'])
+        )
+        return
+
     sid = result_dict['id']
     pid = result_dict['url']
 
     logging.info('schema.org. sid="{}" pid="{}"'.format(sid, pid))
 
     if is_in_gmn(gmn_client, pid):
-      logging.info('Skipped. Already in GMN.')
-      continue
+        logging.info('Skipped. Already in GMN. sid="{}" pid="{}"'.format(sid, pid))
+        return
 
     scimeta_xml_bytes = download_scimeta_xml(result_dict['metadata_url'])
 
     pid_sysmeta_pyxb = generate_system_metadata(scimeta_xml_bytes, pid, sid)
 
+    logging.info('pid_sysmeta_pyxb:')
+    logging.info(d1_common.xml.serialize_to_xml_str(pid_sysmeta_pyxb))
+
     head_sysmeta_pyxb = get_sysmeta(gmn_client, sid)
 
-    # logging.info(sysmeta_pyxb.toxml('utf-8'))
-
     if head_sysmeta_pyxb:
-      head_pid = head_sysmeta_pyxb.identifier.value()
-      logging.info(
-        'SID already exists on GMN. Adding to chain. head_pid="{}"'
-        .format(head_pid)
-      )
-      gmn_client.update(
-        head_pid, io.BytesIO(scimeta_xml_bytes), pid, pid_sysmeta_pyxb
-      )
+        logging.info('head_sysmeta_pyxb:')
+        logging.info(d1_common.xml.serialize_to_xml_str(head_sysmeta_pyxb))
+
+        head_pid = head_sysmeta_pyxb.identifier.value()
+        logging.info(
+            'SID already exists on GMN. Adding to chain. head_pid="{}"'
+                .format(head_pid)
+        )
+        # print('1'*100)
+        gmn_client.update(
+            head_pid, io.BytesIO(scimeta_xml_bytes), pid, pid_sysmeta_pyxb
+        )
+        # print('2'*100)
     else:
-      logging.info(
-        'SID does not exist on GMN. Starting new chain. pid="{}"'.format(pid)
-      )
-      gmn_client.create(pid, io.BytesIO(scimeta_xml_bytes), pid_sysmeta_pyxb)
+        # print('3'*100)
+        logging.info(
+            'SID does not exist on GMN. Starting new chain. pid="{}"'.format(pid)
+        )
+        gmn_client.create(pid, io.BytesIO(scimeta_xml_bytes), pid_sysmeta_pyxb)
+        # print('4'*100)
 
 
 def download_scimeta_xml(scimeta_url):
-  try:
-    return requests.get(scimeta_url).content
-  except requests.HTTPError as e:
-    raise AdapterException(
-      'Unable to download SciMeta. error="{}"'.format(str(e))
-    )
+    try:
+        return requests.get(scimeta_url).content
+    except requests.HTTPError as e:
+        raise AdapterException(
+            'Unable to download SciMeta. error="{}"'.format(str(e))
+        )
 
 
 def parse_doi(iso_xml):
-  """Get the DOI from an ISO XML doc"""
-  tree = ET.parse('iso.xml')
-  root = tree.getroot()
-  doi_el = root.findall(
-    '.gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/'
-    'gmd:CI_Citation/gmd:identifier/gmd:MD_Identifier/gmd:code/'
-    'gco:CharacterString', NS_DICT
-  )[0]
-  return doi_el.text.strip()
+    """Get the DOI from an ISO XML doc"""
+    tree = ET.parse(iso_xml)
+    root = tree.getroot()
+    doi_el = root.findall(
+        '.gmd:identificationInfo/gmd:MD_DataIdentification/gmd:citation/'
+        'gmd:CI_Citation/gmd:identifier/gmd:MD_Identifier/gmd:code/'
+        'gco:CharacterString', NS_DICT
+    )[0]
+    return doi_el.text.strip()
 
 
-def create_gmn_client():
-  return d1_client.mnclient_2_0.MemberNodeClient_2_0(
-    base_url=BASE_URL,
-    cert_pem_path=CERT_PEM_PATH,
-    cert_key_path=CERT_KEY_PATH,
-    retries=1,
-  )
+def create_gmn_client(gmn_base_url, cert_pem_path, cert_key_path):
+    return d1_client.mnclient_2_0.MemberNodeClient_2_0(
+        base_url=gmn_base_url,
+        cert_pem_path=cert_pem_path,
+        cert_key_path=cert_key_path,
+        # retries=1,
+    )
 
 
 def is_in_gmn(gmn_client, did):
-  try:
-    gmn_client.getSystemMetadata(did)
-  except d1_common.types.exceptions.NotFound:
-    return False
-  return True
+    try:
+        gmn_client.getSystemMetadata(did)
+    except d1_common.types.exceptions.NotFound:
+        return False
+    return True
 
 
 def get_sysmeta(gmn_client, sid):
-  try:
-    return gmn_client.getSystemMetadata(sid)
-  except d1_common.types.exceptions.NotFound:
-    return None
+    try:
+        return gmn_client.getSystemMetadata(sid)
+    except d1_common.types.exceptions.NotFound:
+        return None
 
 
 def generate_system_metadata(scimeta_bytes, pid, sid):
-  """
-  :param scimeta_bytes: Bytes of the node's original metadata document.
-  :param native_identifier_sid: Node's system identifier for this object, which
-  becomes the series ID.
-  :param record_date: Date metadata document was created/modified in the source
-  system. Becomes dateUploaded.
-  :param sysmeta_settings_dict: A dict containing node-specific system metadata
-  properties that will apply to all science metadata documents loaded into GMN.
+    sysmeta_pyxb = v2.systemMetadata()
+    sysmeta_pyxb.seriesId = sid
+    sysmeta_pyxb.formatId = SCIMETA_FORMAT_ID
+    sysmeta_pyxb.size = len(scimeta_bytes)
+    sysmeta_pyxb.checksum = d1_common.checksum.create_checksum_object_from_stream(
+        io.BytesIO(scimeta_bytes))
+    sysmeta_pyxb.identifier = pid
+    sysmeta_pyxb.dateUploaded = d1_common.date_time.utc_now()
+    sysmeta_pyxb.dateSysMetadataModified = datetime.datetime.now()
+    sysmeta_pyxb.rightsHolder = SCIMETA_RIGHTS_HOLDER
+    sysmeta_pyxb.submitter = SCIMETA_SUBMITTER
+    sysmeta_pyxb.authoritativeMemberNode = SCIMETA_AUTHORITATIVE_MEMBER_NODE
+    sysmeta_pyxb.originMemberNode = SCIMETA_AUTHORITATIVE_MEMBER_NODE
+    sysmeta_pyxb.accessPolicy = v2.AccessPolicy()
 
-  This function generates a system metadata document for describing the science
-  metadata record being loaded. Some of the fields, such as checksum and size,
-  are based off the bytes of the science metadata object itself. Other system
-  metadata fields are passed to D1ClientManager in a dict which is configured in
-  the main adapter program. Note that the checksum is assigned as an arbitrary
-  version identifier to accommodate the source system's mutable content
-  represented in the target system's immutable content standard.
-  """
-  sysmeta_pyxb = v2.systemMetadata()
-  sysmeta_pyxb.seriesId = sid
-  sysmeta_pyxb.formatId = SCIMETA_FORMAT_ID
-  sysmeta_pyxb.size = len(scimeta_bytes)
-  sysmeta_pyxb.checksum = d1_common.checksum.create_checksum_object_from_string(
-    scimeta_bytes
-  )
-  sysmeta_pyxb.identifier = pid
-  sysmeta_pyxb.dateUploaded = d1_common.date_time.utc_now()
-  sysmeta_pyxb.dateSysMetadataModified = datetime.datetime.now()
-  sysmeta_pyxb.rightsHolder = SCIMETA_RIGHTS_HOLDER
-  sysmeta_pyxb.submitter = SCIMETA_SUBMITTER
-  sysmeta_pyxb.authoritativeMemberNode = SCIMETA_AUTHORITATIVE_MEMBER_NODE
-  sysmeta_pyxb.originMemberNode = SCIMETA_AUTHORITATIVE_MEMBER_NODE
-  sysmeta_pyxb.accessPolicy = v2.AccessPolicy()
+    with d1_common.wrap.access_policy.wrap_sysmeta_pyxb(sysmeta_pyxb) as ap:
+        ap.clear()
+        ap.add_public_read()
 
-  with d1_common.wrap.access_policy.wrap_sysmeta_pyxb(sysmeta_pyxb) as ap:
-    ap.clear()
-    ap.add_public_read()
-
-  return sysmeta_pyxb
+    return sysmeta_pyxb
 
 
 class AdapterException(Exception):
-  pass
+    pass
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
